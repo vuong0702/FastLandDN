d9f0a767f8b03d1668d49c173277d975e8a9f5e6
Shyy <vuong.7.2.2k4@gmail.com>
2025-11-07 10:39:27 +0700

    update API URL in config.js to Render backend

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:4000';
+export const API_URL = 'https://fastlanddn.onrender.com/'; //http://localhost:4000
 export const getApiUrl = (path) => `${API_URL}${path}`;
